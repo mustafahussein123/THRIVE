cf617e1bbb1462a4f44e79644f05f0510632af24
Mustafa Hussein <mustafahussein679@gmail.com>
2025-03-05 21:38:53 -0800

    Add files via upload

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,6 +1,7 @@
+// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home'; // original Home component
 import LocationDetails from './pages/LocationDetails';
 import CompareLocations from './pages/CompareLocations';
 import SavedLocations from './pages/SavedLocations';
@@ -8,10 +9,13 @@ import Onboarding from './pages/Onboarding';
 import OnboardingChoice from './pages/OnboardingChoice';
 import DecisionTree from './pages/DecisionTree';
 import DecisionTreeResults from './pages/DecisionTreeResults';
+import RentalQuestionnaire from './pages/RentalQuestionnaire';
+import RentalRecommendations from './pages/RentalRecommendations';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import MLDashboard from './pages/Admin/MLDashboard';
+import NearbyPlacesSearch from './components/NearbyPlacesSearch';
 import { AuthProvider } from './context/AuthContext';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -29,6 +33,10 @@ function App() {
           <Route path="/onboarding-choice" element={<OnboardingChoice />} />
           <Route path="/decision-tree" element={<DecisionTree />} />
           <Route path="/decision-tree-results" element={<DecisionTreeResults />} />
+          <Route path="/rental-questionnaire" element={<RentalQuestionnaire />} />
+          <Route path="/rental-recommendations" element={<RentalRecommendations />} />
+          {/* New route for Nearby Places Search */}
+          <Route path="/nearby-search" element={<NearbyPlacesSearch />} />
           
           {/* Protected routes */}
           <Route path="/saved" element={<PrivateRoute><SavedLocations /></PrivateRoute>} />
